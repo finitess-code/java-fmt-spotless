c2eb1f3ea098be3d4fa1aadc9b9feb42456a5116
Oleksandr Shmatko <oleksandr.shmatko@finitess.com>
2024-08-19 12:43:39 +0200

    Initial commit - Maven project & unformatted file

---
+++ src/main/java/com/finitess/spotless/UnformattedClassA.java
@@ -0,0 +1,22 @@
+package com.finitess.spotless;
+
+public
+class  UnformattedClassA
+{
+
+    void        hello() {
+//        this should return hello
+        System.
+                out
+                .println("hello");
+
+    }
+
+}
+
+class SomeInnerClassA
+
+
+{
+
+}
